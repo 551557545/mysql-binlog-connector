613bf44b743519627ce1003384ba0abce8c765ed
liuzijian <57895123>
2023-09-18 16:01:45 +0800

    添加事件监听

--- src/main/java/com/cl/mysql/binlog/Main.java
+++ src/main/java/com/cl/mysql/binlog/Main.java
@@ -1,5 +1,7 @@
 package com.cl.mysql.binlog;
 
+import com.cl.mysql.binlog.binlogEvent.Event;
+import com.cl.mysql.binlog.listener.EventListener;
 import com.cl.mysql.binlog.network.MysqlBinLogConnector;
 
 public class Main {
@@ -8,5 +10,26 @@ public class Main {
 //        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/test", "root", "57895123");
         MysqlBinLogConnector connector = MysqlBinLogConnector.openConnect("127.0.0.1", 3307, "root", "57895123", false, null);
         connector.sendComBingLogDump();
+        connector.registerEventListener(new EventListener() {
+            @Override
+            public void listenAll(Event event) {
+                //监听所有事件
+            }
+
+            @Override
+            public void listenUpdateEvent(Event event) {
+                //监听更新事件
+            }
+
+            @Override
+            public void listenDeleteEvent(Event event) {
+                //监听删除事件
+            }
+
+            @Override
+            public void listenInsertEvent(Event event) {
+                //监听新增事件
+            }
+        });
     }
 }
--- src/main/java/com/cl/mysql/binlog/listener/EventListener.java
+++ src/main/java/com/cl/mysql/binlog/listener/EventListener.java
@@ -3,18 +3,34 @@ package com.cl.mysql.binlog.listener;
 import com.cl.mysql.binlog.binlogEvent.Event;
 
 /**
- * @description:
+ * @description: 事件监听者
  * @author: liuzijian
  * @time: 2023-09-18 15:53
  */
 public interface EventListener {
 
+    /**
+     * 监听所有事件
+     * @param event
+     */
     void listenAll(Event event);
 
+    /**
+     * 监听更新事件
+     * @param event
+     */
     void listenUpdateEvent(Event event);
 
+    /**
+     * 监听删除事件
+     * @param event
+     */
     void listenDeleteEvent(Event event);
 
+    /**
+     * 监听新增事件
+     * @param event
+     */
     void listenInsertEvent(Event event);
 
 }
